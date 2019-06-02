83f10d53d8142e479fef3f3a9fa223b93232f772
Christopher W. Butler <cbutler.msstate@gmail.com>
2019-06-02 14:39:45 -0400

    Added public sans font

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Font } from 'expo';
 
 export default class App extends React.Component {
+  state = {
+    fontLoaded: false,
+  };
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'public-sans-bold': require('./assets/public-sans/fonts/webfonts/PublicSans-Bold.ttf'),
+    });
+
+    this.setState({ fontLoaded: true })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        {this.state.fontLoaded && <Text style={styles.text}>Occu</Text>}
       </View>
     );
   }
@@ -14,8 +27,14 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1FDA9A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 68,
+    color: '#fff',
+    fontFamily: 'public-sans-bold',
+  }
 });
